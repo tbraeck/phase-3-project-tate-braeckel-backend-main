397733c3af4044861ff94c3802ceff1341f2bad0
Tate Braeckel <tatebraeckel@gmail.com>
2023-07-10 13:53:51 -0500

    monday

--- db/migrate/20230518024208_create_subjects.rb
+++ db/migrate/20230518024208_create_subjects.rb
@@ -2,12 +2,10 @@ class CreateSubjects < ActiveRecord::Migration[6.1]
   def change
 
     create_table :subjects do |t|
-
       t.string :name
       t.string :description
       t.string :url
       t.timestamps
-
     end
   end
 end
